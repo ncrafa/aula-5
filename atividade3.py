bbad98d54c1dc0ee2298a6580b95c45917f1cda1
ncrafa <69608083+ncrafa@users.noreply.github.com>
2020-09-08 22:55:00 -0300

    atividade3

---
+++ atividade3.py
@@ -0,0 +1,8 @@
+valor=(float(input("Insira um valor a ser encontrado na lista ")))
+lista=[4,5,6,7,8]
+if valor in lista:
+    print("O valor ",valor," foi encontrado")
+else:
+    print("O valor ",valor," não foi encontrado")
+
+    
