1e11a142f5911c0f581259f7d249ba12b520be2b
ncrafa <69608083+ncrafa@users.noreply.github.com>
2020-09-08 23:08:25 -0300

    atividade5

---
+++ atividade5.py
@@ -0,0 +1,6 @@
+listaGamer=["mouse pad","nao sei","CPU","placa mãe","mais alguma coisa","e agora","headset"," cooler master","placa de vídeo GeForce RTX 2080Ti"]
+listaGamer.pop(7)
+listaGamer.pop(7)
+
+print(listaGamer)
+
