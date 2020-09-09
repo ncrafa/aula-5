8615d5b4f7f3164eaf23fc9797efaa6bdc3ec225
ncrafa <69608083+ncrafa@users.noreply.github.com>
2020-09-08 23:08:08 -0300

    atividade4

---
+++ atividade4.py
@@ -0,0 +1,8 @@
+listaFilmes=["exterminador do futuro ","bombox","benjavu"]
+listaJogos=["lol","bomberman "," clash of clans "]
+listaLivros=["nao leio","nao sei"]
+listaEsportes=["beisebol","futebol americano"]
+listaCompleta=listaLivros + listaEsportes + listaJogos + listaFilmes  + listaLivros
+
+print("Um item da tabela de filmes : ",listaFilmes[1]) 
+
